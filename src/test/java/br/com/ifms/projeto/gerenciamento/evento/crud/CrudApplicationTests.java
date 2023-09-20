bc10f4235b46395f20b8c76b85ffbbdeec3e80fb
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-09-19 20:12:28 -0400

    ⚡️ DesenvolvendoSoftware

---
+++ src/test/java/br/com/ifms/projeto/gerenciamento/evento/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifms.projeto.gerenciamento.evento.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
