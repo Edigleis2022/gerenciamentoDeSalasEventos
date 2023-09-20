4d37edaf2ca3413e3b958a8d0517ab89296174cc
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-09-19 21:09:20 -0400

    🚧 Software em Desenvolvimento

---
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Reserva/Reserva.java
@@ -0,0 +1,12 @@
+package br.com.ifms.projeto.gerenciamento.evento.crud.Reserva;
+
+import java.util.Date;
+
+public class Reserva {
+    
+    private Date data;
+    private String nome;
+    private Double valor;
+    private String periodo;
+    
+}
---
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Reserva/ReservaController.java
@@ -0,0 +1,5 @@
+package br.com.ifms.projeto.gerenciamento.evento.crud.Reserva;
+
+public class ReservaController {
+    
+}
