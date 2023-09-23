c0c3defb8d161a9c82397305020fa86c7b89a6f7
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-09-22 20:29:56 -0400

    🚧 Código em Desenvolvimento

--- src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/Associado.java
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/Associado.java
@@ -6,9 +6,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Associado {
     
-    private Long cpf;
+    //Atributo
+    private Integer cpf;
     private String nome;
     private Integer quadraLote;
+
+    //Get e Set
+    public Integer getCpf() {
+        return cpf;
+    }
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getQuadraLote() {
+        return quadraLote;
+    }
+    public void setQuadraLote(Integer quadraLote) {
+        this.quadraLote = quadraLote;
+    }
 
     
 
--- src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/AssociadoController.java
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/AssociadoController.java
@@ -2,6 +2,8 @@ package br.com.ifms.projeto.gerenciamento.evento.crud.Associado;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,6 +19,11 @@ public class AssociadoController {
     @GetMapping("/boasVindas/{nome}")
     public String boasVindas(@PathVariable String nome){
         return "Seja bem Vindo Associado(a) " + nome + " !!! ";
+    }
+
+    @PostMapping("/associado")
+    public Associado associado(@RequestBody Associado socio){
+        return socio;
     }
 
 }
