d3314f2ba8eb68b0a5c48e7c607a40218f502e11
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-09-25 12:47:39 -0400

    🚧 Em desenvolvimento 25/09

--- src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/Associado.java
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/Associado.java
@@ -1,36 +1,57 @@
 package br.com.ifms.projeto.gerenciamento.evento.crud.Associado;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@RestController
+
+@Entity
+@Table(name = "associados")
 public class Associado {
-    
-    //Atributo
+
+    // Atributo
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int condigo;
     private String cpf;
     private String nome;
     private String quadraLote;
 
-    //Get e Set
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getQuadraLote() {
-        return quadraLote;
-    }
-    public void setQuadraLote(String quadraLote) {
-        this.quadraLote = quadraLote;
+    // Get e Set
+    public int getCondigo() {
+        return condigo;
     }
 
     
+    public void setCondigo(int condigo) {
+        this.condigo = condigo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getQuadraLote() {
+        return quadraLote;
+    }
+
+    public void setQuadraLote(String quadraLote) {
+        this.quadraLote = quadraLote;
+    }
 
 }
