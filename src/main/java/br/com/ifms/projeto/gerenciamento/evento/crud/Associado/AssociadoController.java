180f71011c20aef1e0bfa72284827715cb3f8e46
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-10-01 19:21:44 -0400

    🚧 Novas atualizações 01/10/2023 19:21

--- src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/AssociadoController.java
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/AssociadoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,14 +32,22 @@ public class AssociadoController {
         return acao.save(obj);
     }
 
+    /*Este method lista os dados do Banco de Dados*/
     @GetMapping("/api")
     public List<Associado> selecionar(){
         return acao.findAll();
     }
 
+    /*Este metodo filtra os dados do Banco de dados */
     @GetMapping("/api/{id}")
     public Associado selecionarPeloId(@PathVariable Long id){
         return acao.findByid(id);
+    }
+
+    /* Este metodo retorna um objeto  */
+    @PutMapping("/api")
+    public Associado editar(@RequestBody Associado obj){
+        return acao.save(obj);
     }
 
     @GetMapping("")
