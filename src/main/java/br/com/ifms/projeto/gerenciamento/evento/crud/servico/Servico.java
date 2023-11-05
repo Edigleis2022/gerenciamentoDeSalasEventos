f1dde43cf07f9448de246dfc40e8c4a40b05b21e
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-11-05 15:41:36 -0400

    🚧 Atualizado 05/11/2023 15:41

---
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/servico/Servico.java
@@ -0,0 +1,10 @@
+package br.com.ifms.projeto.gerenciamento.evento.crud.servico;
+
+import org.springframework.stereotype.Service;
+
+/*Aqui é o local onde ficaram 
+  as regras de negócios*/
+@Service
+public class Servico {
+    
+}
