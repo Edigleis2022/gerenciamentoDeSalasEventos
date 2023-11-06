eb4d7b2f20856401fbb8a1df4589a660c402332b
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-11-06 10:49:57 -0400

    🚧 Atualiado 06/11/2023 10:49

--- src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/Mensagem.java
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/Associado/Mensagem.java
@@ -2,6 +2,9 @@ package br.com.ifms.projeto.gerenciamento.evento.crud.Associado;
 
 import org.springframework.stereotype.Component;
 
+/*A annotecion Component vai varrer, 
+  pesquisar e procurar todos os elementos 
+  que estiverem dentro dessa classe mensagem */
 @Component
 public class Mensagem {
     
