5198e7c8a1a0a229d7f4c09f4c040e32f2b42dbb
Edigleis2022 <edigleisbuiu@hotmail.com>
2023-11-19 08:21:56 -0400

    ✨ IMPLEMENTANDO A DTO

--- src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/dto/EspacoitemDTO.java
+++ src/main/java/br/com/ifms/projeto/gerenciamento/evento/crud/dto/EspacoitemDTO.java
@@ -2,5 +2,67 @@ package br.com.ifms.projeto.gerenciamento.evento.crud.dto;
 
 import java.io.Serializable;
 
-public class EspacoitemDTO implements Serializable {
+import br.com.ifms.projeto.gerenciamento.evento.crud.entities.Espaco;
+import br.com.ifms.projeto.gerenciamento.evento.crud.entities.EspacoItem;
+import br.com.ifms.projeto.gerenciamento.evento.crud.entities.enums.Situacao;
+
+public class EspacoItemDTO implements Serializable {
         private static final long serialVersionUID = 1l;
+
+        private Long id;
+        private String numeroSerie;
+        private Situacao situacao;
+        private Espaco espaco;
+
+         public EspacoItemDTO(){
+            //TODO Auto-generated constructor stub
+        }
+        
+        public EspacoItemDTO(Long id, String numeroSerie, Situacao situacao, Espaco espaco){
+            this.id = id;
+            this.numeroSerie = numeroSerie;
+            this.situacao = situacao;
+            this.espaco = espaco;
+        }
+        
+        public EspacoItemDTO(EspacoItem entity){
+            this.id = entity.getId();
+            this.numeroSerie = entity.getNumeroSerie();
+            this.situacao = entity.getSituacao();
+            this.espaco = entity.getEspaco();
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNumeroSerie() {
+            return numeroSerie;
+        }
+
+        public void setNumeroSerie(String numeroSerie) {
+            this.numeroSerie = numeroSerie;
+        }
+
+        public Situacao getSituacao() {
+            return situacao;
+        }
+
+        public void setSituacao(Situacao situacao) {
+            this.situacao = situacao;
+        }
+
+        public Espaco getEspaco() {
+            return espaco;
+        }
+
+        public void setEspaco(Espaco espaco) {
+            this.espaco = espaco;
+        }
+
+       
+}
